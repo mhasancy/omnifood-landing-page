44990cd61259109949808b536a55e75b20d5f666
Mahadi Hasan <mhasancy@gmail.com>
2022-07-14 02:56:13 +0600

    added sticky nav with js

--- js/script.js
+++ js/script.js
@@ -47,6 +47,27 @@ if (link.classList.contain("main-nav-link")) {
   headerEl.classList.toggle("nav-open");
 }
 
+////////////////////////////////
+//Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flex-box gap property missing in some Safari versions
 function checkFlexGap() {
